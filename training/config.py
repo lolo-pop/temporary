2010af9d4d5fe94061211ecb87a716f658fe52df
lolo-pop <sonwoine@hotmail.com>
2023-04-27 21:39:54 +0800

    0427

--- training/config.py
+++ training/config.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-evaluation_quantile = [i/20 for i in range(1,20)] + [0.99]
+evaluation_quantile = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 prediction_length = [20, 40, 60, 2*60, 3*60, 5*60, 10*60, 20*60, 30*60] # seconds
 baseline_model = ["DeepAR", "Transformer", "TFT", "FeedforwardNN", "DeepFactor", "MQRNN"]
 train_model = ["DeepAR", "Transformer", "TFT", "FeedforwardNN", "DeepFactor", "MQRNN"] # 用于指定这次要训练的模型
@@ -9,11 +9,11 @@ evaluate_model = ["TFT"] # 用于指定这次要测试的模型
 
 
 dataset_meta_data = {
-        "data_path" : Path("/home/rongch05/openfaas/AzurePublicDataset/invocation.csv"),
-        "model_path" : Path("predict_models/"),
+        "data_path" : Path("/home/rongch05/openfaas/AzurePublicDataset/invocationTraceByAppID.csv"),
+        "model_path" : Path("predict_models_by_appID/"),
         "evaluate_result_path": Path("results/"),
-        "point_result_path": Path("point results/"),
-        "train_ds_ratio" : 0.9,
+        "point_result_path": Path("point_results/"),
+        "train_ds_ratio" : 0.95,
         "prediction_length" : 1,   # 预测下一个时间窗口取值 1 
         "frequency" : "30s"
     }
---
+++ training/test.py
@@ -0,0 +1,28 @@
+from gluonts.model.predictor import Predictor
+from gluonts.dataset.field_names import FieldName
+import numpy as np
+from pathlib import Path
+import pandas as pd
+from gluonts.dataset.common import ListDataset
+# Load the pre-trained model
+predictor = Predictor.deserialize(Path("/home/rongch05/openfaas/training/predict_models/TFT"))
+
+# Define the monitoring sequence
+monitoring_sequence = np.array([38,48,50,26,20,25,24,42,28,27,27,35,40])
+# 140,503,411,388,320,295,288,150,203,73,735,577,436,378,469,307,110,95,140
+# Convert the monitoring sequence to GluonTS format
+print(pd.to_datetime(0, unit="s"))
+monitoring_data = ListDataset (data_iter =
+                               [
+                                 {FieldName.START: pd.to_datetime(0, unit="s"), FieldName.TARGET: monitoring_sequence}
+                                 ],  
+                               freq = "30s"
+                               )
+
+# Make predictions for the next time step
+forecast_it = predictor.predict(dataset=monitoring_data, num_samples=100)
+
+# Print the predicted value for the next time step
+forecasts = list(forecast_it)
+print(forecasts)
+print(f"Predicted value: {forecasts[0].samples.mean(axis=0)[-1]}")
